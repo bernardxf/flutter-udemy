b080db17d7c2f3a4e941b1942955283286b02468
Bernard Faria <bernardxf@gmail.com>
2019-04-21 15:01:59 -0300

    Flutter Tube

---
+++ flutter_tube/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tube/api.dart';
+import 'package:flutter_tube/blocs/favorite_bloc.dart';
+import 'package:flutter_tube/blocs/videos_bloc.dart';
+import 'package:flutter_tube/screens/home.dart';
+
+void main() {
+
+  Api api = Api();
+  api.search("eletro");
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: VideosBloc(),
+      child: BlocProvider(
+        bloc: FavoriteBloc(),
+          child: MaterialApp(
+            title: 'FlutterTube',
+            debugShowCheckedModeBanner: false,
+            home: Home(),
+          ),
+      )
+    );
+  }
+}
+
